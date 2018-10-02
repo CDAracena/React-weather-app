6e473efce293ce6c28de1794d0f71ab14fb0c68a
Christian Aracena <CDAracena@gmail.com>
2018-10-02 10:29:11 -0400

    model-card complete

--- src/City.js
+++ src/City.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './index.css';
 
-const City = ({name, currentTemp, imgSrc, description, humidity})=> {
-
-  return(
-    <div className="city-card-container">
-      <div className="city-name">
-        {name}
-      </div>
-      <div className="city-icon">
-        {imgSrc}
-      </div>
-      <div className="city-weather">
-        {currentTemp}, Humidity: {humidity}
-      </div>
+const City = ({name, currentTemp, imgSrc, description, humidity}) => {
+  return (<div className="city-card-container">
+    <div className="city-name">
+      {name}
     </div>
-  )
+    <div className="city-icon">
+      <img src={imgSrc} alt={description}/>
+    </div>
+    <div className="city-weather">
+      {currentTemp}, {humidity}
+    </div>
+    <div className="city-description">
+      {description}
+    </div>
+  </div>)
 }
 
 export default City;
